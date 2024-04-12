f2f7e9a75b4eb4cb31c71deec3a426b045a1959e
Songhyun Bae <shyunb21@gmail.com>
2024-04-12 11:22:28 +0900

    week1:assign - task1 타마고 게임 제작

---
+++ Week1/main.js
@@ -0,0 +1,21 @@
+const num = Number(prompt("숫자를 입력하세요!"));
+
+const cnt1 = document.getElementById("cnt1");
+const egg1 = document.getElementById("egg1");
+
+cnt1.innerHTML = num;
+
+egg_num = num;
+
+const hit = () => {	
+	if (egg_num <= 1) {
+        egg_num = 0;
+        cnt1.innerHTML = egg_num;
+		alert("성공!");
+        return;
+	}
+    egg_num--;	
+    cnt1.innerHTML = egg_num;
+};
+
+egg1.addEventListener("click", hit);
