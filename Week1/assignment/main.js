e28d29aa6fda3ab84ee3b79004b5f6dcc94d7dfd
Songhyun Bae <shyunb21@gmail.com>
2024-04-12 16:20:49 +0900

    week1:assign - task4 알을 두개를 만들어서

--- Week1/assignment/main.js
+++ Week1/assignment/main.js
@@ -2,22 +2,36 @@ const num = Number(prompt("숫자를 입력하세요!"));
 
 const cnt1 = document.getElementById("cnt1");
 const egg1 = document.getElementById("egg1");
+const cnt2 = document.getElementById("cnt2");
+const egg2 = document.getElementById("egg2");
 
 cnt1.innerHTML = num;
+cnt2.innerHTML = num;
 
-let egg_num = num;
+let egg_num = [num, num];
 const color = ["red", "orange", "yellow", "green", "blue", "navy", "purple"];
 
-const hit = () => {	
-	if (egg_num <= 1) {
-        egg_num = 0;
-        cnt1.innerHTML = egg_num;
+const hit = (n) => {	
+    if(n==1) {
+        temp = cnt1;
+    } else {
+        temp = cnt2;
+    }
+
+	if (egg_num[n-1] <= 1) {
+        egg_num[n-1] = 0;
+        temp.innerHTML = egg_num[n-1];
 		alert("성공!");
         return;
 	}
-    egg_num--;	
-    cnt1.style.color = color[Math.floor(((num - egg_num) / num) * 7)];
-    cnt1.innerHTML = egg_num;
+    egg_num[n-1]--;	
+    temp.style.color = color[Math.floor(((num - egg_num[n-1]) / num) * 7)];
+    temp.innerHTML = egg_num[n-1];
 };
 
-egg1.addEventListener("click", hit);
+egg1.addEventListener("click", function() {
+    hit(1)
+});
+egg2.addEventListener("click", function() {
+    hit(2)
+});
--- Week1/assignment/task2.js
+++ Week1/assignment/task2.js
@@ -1,4 +1,11 @@
-while (egg_num > 0) {
-    egg = document.getElementById("egg1");
-    egg.click()
+// while (egg_num > 0) {
+//     egg = document.getElementById("egg1");
+//     egg.click()
+// }
+
+while (egg_num[1] > 0 || egg_num[0] > 0) {
+    one = document.getElementById("egg1");
+    two = document.getElementById("egg2");
+    one.click();
+    two.click();
 }
